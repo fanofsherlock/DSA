85d3684771cafa730a51ad8d06dab42f524d4c20
fanofsherlock <mailme.shivam21@gmail.com>
2019-12-19 14:07:09 +0530

    Performance

---
+++ Java_Performace/src/JIT/Main.java
@@ -0,0 +1,13 @@
+package JIT;
+
+public class Main {
+
+	public static void main(String[] args) throws InterruptedException {
+		Thread.sleep(20000);
+		System.out.println("starting the work...");
+		PrimeNumbers primeNumbers = new PrimeNumbers();
+		Integer max = Integer.parseInt(args[0]);
+		primeNumbers.generateNumbers(max);
+	}
+
+}
