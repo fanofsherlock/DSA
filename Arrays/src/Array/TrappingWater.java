62a3458bf10fcc95574bf38c1e95d0995bccb90e
fanofsherlock <mailme.shivam21@gmail.com>
2020-02-07 01:36:21 +0530

    trapping rain water

--- Arrays/src/Array/TrappingWater.java
+++ Arrays/src/Array/TrappingWater.java
@@ -2,21 +2,38 @@ package Array;
 
 public class TrappingWater {
 
-	public static void main(String[] args) {
-		int[] arr1= {7,4,0,9};
-		int[] arr2= {6,9,9};
-		
-		tpWater(arr1);
-		tpWater(arr1);
-	}
-
-	private static void tpWater(int[] arr1) {
-		int trappedWater=0;
-		int maxSoFar= Integer.MIN_VALUE;
-		
-		for(int i=0;i<arr1.length;i++) {
-		     
-		}
-		
-	}
+	public int trap(int[] height) {
+        if(height==null||height.length==0){
+            return 0;
+        }
+        int[] leftHigh= new int[height.length];
+        int[] rightHigh= new int[height.length];
+        
+        leftHigh[0]=height[0];
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<height.length;i++){
+            if(max<height[i]){
+                max=height[i];
+            }
+            leftHigh[i]=max;
+        }
+        
+        rightHigh[height.length-1]=height[height.length-1];
+        max=Integer.MIN_VALUE;
+        for(int i=height.length-1;i>=0;i--){
+            if(max<height[i]){
+                max=height[i];
+            }
+            rightHigh[i]=max;
+        }
+        
+        int maxRain=0;
+        for(int i=0;i<height.length;i++){
+            maxRain+=Math.min(leftHigh[i],rightHigh[i])-height[i];
+            
+        }
+        
+        return maxRain;
+        
+    }
 }
